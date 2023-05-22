3caca0c1eb436ae77b17ae3051eb030cb679f540
Bhavya Thumar <thumarbhavya2003@gmail.com>
2023-05-22 18:08:46 +0530

    Initial Coomit

--- README.md
+++ README.md
@@ -1,34 +1,29 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Todo List Project
 
-## Getting Started
+This is a simple Todo List project created using Next.js, HTML, and CSS. It allows users to manage their tasks by adding, editing, and deleting items from the list.
 
-First, run the development server:
+Features
+Add a new task: Users can enter a task description and press the "Add" button to add it to the list.
+Edit a task: Users can click on the task to enter edit mode and modify the task description.
+Delete a task: Users can click on the delete icon next to a task to remove it from the list.
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+Getting Started
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+To get started with the Todo List project, follow these steps:
+Clone the repository: git clone <repository-url>
+Navigate to the project directory: cd todo-list-project
+Install the dependencies: npm install
+Start the development server: npm run dev
+Open your browser and visit: http://localhost:3000
+Project Structure
+The project structure is organized as follows:
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+Technologies Used
+The Todo List project is built using the following technologies:
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Next.js: A React framework for building server-side rendered and static websites.
+HTML: The standard markup language for creating web pages.
+CSS: The style sheet language used for styling the web pages.
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+License
+This project is licensed under the MIT License. Feel free to use and modify it for your own purposes.
---
+++ pages/todos.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+
+const todos = () => {
+    // const todos = [{ title: "zdfz", desc: "123"}, {title: "123", desc: "sdfasfasdf"}];
+
+    const  [todos, setTodos] = useState([])
+    useEffect(() => {
+        let todos = localStorage.getItem("todos")
+        setTodos(JSON.parse(todos))
+
+    },[])
+
+    const deleteTodo = (title) => {
+        let newTodos = todos.filter(item =>{
+            return item.title != title
+        })
+        localStorage.setItem("todos",JSON.stringify(newTodos))
+        setTodos(newTodos)
+    }
+
+    const editTodo = () => {
+
+    }
+    return (
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-24 mx-auto">
+                <div class="flex flex-col text-center w-full mb-20">
+                    <h1 class="text-4xl font-medium title-font mb-4 text-gray-900">
+                        Your TO-DOs
+                    </h1>
+                    {todos. length == 0 &&<p class="lg:w-2/3 mx-auto leading-relaxed text-base">TO-DO</p>}
+                </div>
+                <div class="flex flex-wrap -m-4">
+                    {todos.map((item) => {
+                        return (
+                            <div class="p-4 lg:w-1/4 md:w-1/2">
+                                <div class="h-full flex flex-col items-center text-center">
+                                    <img
+                                        alt="team"
+                                        class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
+                                        src={`https://picsum.photos/2000/3000?a=${item.title}`}
+                                    />
+                                    <div class="w-full">
+                                        <h2 class="title-font font-medium text-lg text-gray-900">
+                                            {item.title}
+                                        </h2>
+                                        <p class="mb-4">{item.desc}</p>
+                                        <span class="inline-flex">
+                                            <a class="text-gray-500 cursor-pointer" onClick={()=>{deleteTodo(item.title)}} >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    width="20px"
+                                                    height="24px"
+                                                >
+                                                    <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z" />
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500 cursor-pointer" href = {`/edit/${item.title}`}>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 48 48"
+                                                    width="20px"
+                                                    height="24px"
+                                                >
+                                                    <path
+                                                        fill="#E57373"
+                                                        d="M42.583,9.067l-3.651-3.65c-0.555-0.556-1.459-0.556-2.015,0l-1.718,1.72l5.664,5.664l1.72-1.718C43.139,10.526,43.139,9.625,42.583,9.067"
+                                                    />
+                                                    <path
+                                                        fill="#FF9800"
+                                                        d="M4.465 21.524H40.471999999999994V29.535H4.465z"
+                                                        transform="rotate(134.999 22.469 25.53)"
+                                                    />
+                                                    <path
+                                                        fill="#B0BEC5"
+                                                        d="M34.61 7.379H38.616V15.392H34.61z"
+                                                        transform="rotate(-45.02 36.61 11.385)"
+                                                    />
+                                                    <path
+                                                        fill="#FFC107"
+                                                        d="M6.905 35.43L5 43 12.571 41.094z"
+                                                    />
+                                                    <path
+                                                        fill="#37474F"
+                                                        d="M5.965 39.172L5 43 8.827 42.035z"
+                                                    />
+                                                </svg>
+                                            </a>
+                                            <a class="ml-2 text-gray-500">
+                                                {/* <svg
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    stroke-linecap="round"
+                                                    stroke-linejoin="round"
+                                                    stroke-width="2"
+                                                    class="w-5 h-5"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                                </svg> */}
+                                            </a>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    );
+};
+export default todos;
